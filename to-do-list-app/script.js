71b3ed4f6fbcdd1695b489414c59e0a1df63498b
Adiba Anjum <adeeba.anjum3@gmail.com>
2024-10-12 12:17:47 +0530

    added list item deletion and completion displaying functionality

--- to-do-list-app/script.js
+++ to-do-list-app/script.js
@@ -13,5 +13,13 @@ function addTask(){
     span.innerHTML = '\u00d7';
     li.appendChild(span);
   }
-  inputBox.value = '';
+  inputBox.value = "";
 }
+
+listContainer.addEventListener("click", function(e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+  }
+}, false);
