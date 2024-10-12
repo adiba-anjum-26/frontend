c246fa024149f8fcd37e8f2f6c9a9f2cdff5c4c0
Adiba Anjum <adeeba.anjum3@gmail.com>
2024-10-12 12:22:44 +0530

    added local storage capability

--- to-do-list-app/script.js
+++ to-do-list-app/script.js
@@ -14,12 +14,26 @@ function addTask(){
     li.appendChild(span);
   }
   inputBox.value = "";
+  saveData();
 }
 
 listContainer.addEventListener("click", function(e) {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
-  } else if (e.target.tagName === "SPAN") {
+    saveData();
+  } 
+  else if (e.target.tagName === "SPAN") {
     e.target.parentElement.remove();
+    saveData();
   }
 }, false);
+
+function saveData(){
+  localStorage.setItem("data" , listContainer.innerHTML);
+}
+
+function getData(){
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+getData();
